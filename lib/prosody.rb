34fb576b1cf5a2ba9adbd954739f732bd0fc4773
Griffith, Joshua <Joshua.Griffith@fnf.com>
2025-04-28 13:09:01 -0500

    chore: try to build gem (#1)

--- lib/prosody.rb
+++ lib/prosody.rb
@@ -5,10 +5,14 @@ require_relative "prosody/version"
 require_relative "prosody/configuration"
 require_relative "prosody/handler"
 require_relative "prosody/processor"
-require_relative "prosody/prosody"
+
+begin
+  ruby_version = /(\d+\.\d+)/.match(RUBY_VERSION)
+  require_relative "prosody/#{ruby_version}/prosody"
+rescue LoadError
+  require_relative "prosody/prosody"
+end
 
 module Prosody
   class Error < StandardError; end
-
-  # Your code goes here...
 end
