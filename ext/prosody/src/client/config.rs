6efe4a361c5bc90654e8a6374c44ca2946830858
Griffith, Joshua <Joshua.Griffith@fnf.com>
2025-06-11 15:47:45 -0500

    feat: support Configuration objects in Client.new and make probe_port optional (#11)

--- ext/prosody/src/client/config.rs
+++ ext/prosody/src/client/config.rs
@@ -86,7 +86,7 @@ pub struct NativeConfiguration {
     failure_topic: Option<String>,
 
     /// Configuration for the health probe port
-    probe_port: ProbePort,
+    probe_port: Option<ProbePort>,
 }
 
 /// Configuration for the health probe port.
@@ -271,13 +271,15 @@ impl<'a> From<&'a NativeConfiguration> for ConsumerConfigurationBuilder {
             builder.mock(*mock);
         }
 
-        match &config.probe_port {
-            ProbePort::Unconfigured => {}
-            ProbePort::Disabled => {
-                builder.probe_port(None);
-            }
-            ProbePort::Configured(port) => {
-                builder.probe_port(*port);
+        if let Some(probe_port) = &config.probe_port {
+            match probe_port {
+                ProbePort::Unconfigured => {}
+                ProbePort::Disabled => {
+                    builder.probe_port(None);
+                }
+                ProbePort::Configured(port) => {
+                    builder.probe_port(*port);
+                }
             }
         }
 
