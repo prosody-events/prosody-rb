6ae4954ce6e1f368340e274d22f19b7b87c10b10
Griffith, Joshua <Joshua.Griffith@fnf.com>
2025-12-16 17:07:28 -0600

    chore(main): release 2.0.0 (#39)

--- lib/prosody/version.rb
+++ lib/prosody/version.rb
@@ -6,5 +6,5 @@ module Prosody
   # This version number follows semantic versioning and is used by the
   # gem system to identify the library version. It should be updated
   # according to semver guidelines when making releases.
-  VERSION = '1.1.4'
+  VERSION = '2.0.0'
 end
