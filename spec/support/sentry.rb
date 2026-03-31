0150b4c92f72691a484665a717408ee5fed8a584
Joshua Griffith <Joshua.Griffith@fnf.com>
2026-03-31 14:44:02 -0500

    fix: auto-initialize Sentry from SENTRY_DSN when sentry-ruby is installed

--- spec/support/sentry.rb
+++ spec/support/sentry.rb
@@ -1,9 +1,3 @@
 # frozen_string_literal: true
 
-if ENV["SENTRY_DSN"]
-  require "sentry-ruby"
-
-  Sentry.init do |config|
-    config.dsn = ENV["SENTRY_DSN"]
-  end
-end
+require "sentry-ruby" if ENV["SENTRY_DSN"]
