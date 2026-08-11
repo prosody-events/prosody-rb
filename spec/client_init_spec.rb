eb340ae88a38686275dcf130b19932dd5eb0a833
Joshua Griffith <josh.griffith@gmail.com>
2026-08-10 23:51:54 -0500

    test: shut down every client fixture

--- spec/client_init_spec.rb
+++ spec/client_init_spec.rb
@@ -3,15 +3,16 @@
 require "spec_helper"
 
 RSpec.describe Prosody::Client, integration: true do
+  after { @client&.shutdown }
+
   it "initializes a client when given minimal producer configuration as hash" do
     # Create the client with a hash directly
-    client = Prosody::Client.new(
+    @client = Prosody::Client.new(
       bootstrap_servers: TestConfig::BOOTSTRAP_SERVERS,
       source_system: "init-test-system"
     )
 
-    # The client should be instantiated successfully
-    expect(client).to be_a(Prosody::Client)
+    expect(@client).to be_a(Prosody::Client)
   end
 
   it "initializes a client when given a Configuration object" do
@@ -25,9 +26,8 @@ RSpec.describe Prosody::Client, integration: true do
     expect(config.bootstrap_servers).to eq([TestConfig::BOOTSTRAP_SERVERS])
 
     # Create the client with the Configuration object
-    client = Prosody::Client.new(config)
+    @client = Prosody::Client.new(config)
 
-    # The client should be instantiated successfully
-    expect(client).to be_a(Prosody::Client)
+    expect(@client).to be_a(Prosody::Client)
   end
 end
